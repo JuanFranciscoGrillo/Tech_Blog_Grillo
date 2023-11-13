c525461fdafd915cec9af5d28abc60ee9e37da18
Juan Francisco Grillo <jfrgm1986@gmail.com>
2023-11-13 06:43:08 -0400

    Define Schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,8 @@
+-- Drop the database if it already exists
+DROP DATABASE IF EXISTS tech_blog_Grillo;
+
+-- Create the database
+CREATE DATABASE tech_blog_Grillo;
+
+-- Use the database
+USE tech_blog_Grillo;
