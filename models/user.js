6912b8d15244e64573e02b15540a09aed8b33635
Juan Francisco Grillo <jfrgm1986@gmail.com>
2023-11-13 03:44:18 -0400

    Models Created and Defined

--- models/user.js
+++ models/user.js
@@ -4,15 +4,26 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
+     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
+     static associate(models) {
+      // User has many Comments with a foreign key 'userId' and cascade delete
+      User.hasMany(models.Comment, {
+        foreignKey: 'userId',
+        onDelete: 'CASCADE'
+      });
+
+      // User has many Posts with a foreign key 'userId' and cascade delete
+      User.hasMany(models.Post, {
+        foreignKey: 'userId',
+        onDelete: 'CASCADE'
+      });
     }
   }
+
   User.init({
     username: DataTypes.STRING,
     password: DataTypes.STRING
@@ -20,5 +31,6 @@ module.exports = (sequelize, DataTypes) => {
     sequelize,
     modelName: 'User',
   });
+
   return User;
 };
