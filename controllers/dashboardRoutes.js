beb6ae66a5e3c498d2c037c4ce62e3f117e93d81
Juan Francisco Grillo <jfrgm1986@gmail.com>
2023-11-13 07:19:31 -0400

    Generated dashboardRoutes

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,42 @@
+const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth'); // Replace with your actual authentication middleware
+
+// Get dashboard page
+router.get('/', withAuth, async (req, res) => {
+    try {
+        // Fetch posts specific to the logged-in user
+        const userPosts = await Post.findAll({
+            where: {
+                userId: req.session.userId // Assuming the session contains the user's ID
+            },
+            include: [
+                {
+                    model: User,
+                    attributes: ['username']
+                },
+                {
+                    model: Comment,
+                    attributes: ['commentText', 'createdAt'],
+                    include: {
+                        model: User,
+                        attributes: ['username']
+                    }
+                }
+            ]
+        });
+
+        // Serialize data so the template can read it
+        const posts = userPosts.map(post => post.get({ plain: true }));
+
+        // Render the dashboard page with the user's posts
+        res.render('dashboard', { // Replace 'dashboard' with your actual dashboard view file
+            posts,
+            loggedIn: req.session.loggedIn // Assuming you track login status in the session
+        });
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+module.exports = router;
